b7719f80b3f0dddc0d40aa2b5a3c510ccd5bdd1a
Steve Tingiris <steve@dabblelab.com>
2020-10-13 12:52:56 -0400

    Update hello-world.js

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -1,6 +1,6 @@
 exports.handler = function(context, event, callback) {
     const result = {
-        message : "Hello Universe!"
+        message : "Hello World!"
     }
 
     callback(null, result);
